8bed895dcdf598bf8eb31de4edc3c5b4c21444c9
GrSpirit <gr.spirit@gmail.com>
2016-12-10 12:41:17 +0300

    Changed Home title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title on a oer-page basis
+	def full_title(page_title = '')
+		base_title = "Demo App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
