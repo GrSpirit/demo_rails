f9b26a81b7b7b8ff5cfdfc8e685df43fc5a584dd
GrSpirit <gr.spirit@gmail.com>
2016-12-12 13:11:13 +0300

    User email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,11 +44,18 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do 
 		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-													foo@bar_baz.com foo@bar+baz.com]
+													foo@bar_baz.com foo@bar+baz.com user@foo..bar]
 		invalid_addresses << "us er@foo.bar" << "user@foo bar"
 		invalid_addresses.each do |invalid_address|
 			@user.email = invalid_address
 			assert_not @user.valid?, "#{invalid_address} should be invalid"
 		end
+  end
+
+  test "email addresses should be unique" do
+  	duplicate_user = @user.dup
+  	duplicate_user.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate_user.valid?
   end
 end
