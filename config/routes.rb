97d3ae47d5d25646ce3c4c228d5174f4bef52103
GrSpirit <gr.spirit@gmail.com>
2016-12-13 22:07:52 +0300

    Add log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root  'static_pages#home'
+
+  # Static pages
+  get     '/help',    to: 'static_pages#help'
+  get     '/about',   to: 'static_pages#about'
+  get     '/contact', to: 'static_pages#contact'
+
+  # Users
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  resources :users
+
+  # Sessions
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
 end
