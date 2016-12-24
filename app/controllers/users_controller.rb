17deff616f3a862284c78c7fc5ecd61d3cf22fc6
Vitaly <gr.spirit@gmail.com>
2016-12-24 22:11:02 +0300

    Simple authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -40,6 +39,8 @@ class UsersController < ApplicationController
                                    :password_confirmation)
   	end
 
+    # Before filters
+
     # Confirm a logged-in user
     def logged_in_user
       unless logged_in?
@@ -47,4 +48,11 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+
+    # Confirm the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
